d19bab5972f355e3f3f61a6e4dfbe13de630a57c
anjalikalra1975 <anjali.kalraa2007@gmail.com>
2025-09-04 22:14:22 +0530

    Add files via upload

---
+++ DayOneFirstQuestion.c
@@ -0,0 +1,13 @@
+//Q1: Write a program to input two numbers and display their sum
+#include<stdio.h>
+int main() {
+int a,b;
+printf("Enter your first number:");
+scanf("%d",&a);
+printf("Enter your second number:");
+scanf("%d",&b);
+int sum;
+sum= a+b;
+printf("Your sum: %d",sum);
+return 0;
+}

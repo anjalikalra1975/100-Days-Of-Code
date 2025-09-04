89c5d336569dbf06cfb01699e74a916aefba6034
anjalikalra1975 <anjali.kalraa2007@gmail.com>
2025-09-04 22:09:37 +0530

    Add files via upload

---
+++ DayOneFirstQuestion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+int a,b;
+printf("Enter your first number:");
+scanf("%d",&a);
+printf("Enter your second number:");
+scanf("%d",&b);
+int sum;
+sum= a+b;
+printf("Your sum: %d",sum);
+return 0;
+}

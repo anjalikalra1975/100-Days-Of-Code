fb4a0b2513efab8b2adde8c1e3f73f057f4b0c71
anjalikalra1975 <anjali.kalraa2007@gmail.com>
2025-10-01 00:07:06 +0530

    Add files via upload

---
+++ DayElevenQuestionOne.c
@@ -0,0 +1,49 @@
+//Q21:Write a program to display the month name and number of days using switch-case for a given month number.
+#include<stdio.h>
+int main() {
+    int num;
+    printf("Enter a number(1-12):");
+    scanf("%d",&num);
+    switch (num) {
+    case 1:
+    printf("The month is January.");
+    break;
+    case 2:
+    printf("The month is February.");
+    break;
+    case 3:
+    printf("The month is March.");
+    break;
+    case 4:
+    printf("The month is April.");
+    break;
+    case 5:
+    printf("The month is May.");
+    break;
+    case 6:
+    printf("The month is June.");
+    break;
+    case 7:
+    printf("The month is July.");
+    break;
+    case 8:
+    printf("The month is August.");
+    break;
+    case 9:
+    printf("The month is September.");
+    break;
+    case 10:
+    printf("The month is October.");
+    break;
+    case 11:
+    printf("The month is November.");
+    break;
+    case 12:
+    printf("The month is December.");
+    break;
+    default:
+    printf("Incorrect input.");
+    break;
+    }
+    return 0;
+}

b23e21fcaf12d0d5c9e6abde3354ccc727dd441f
anjalikalra1975 <anjali.kalraa2007@gmail.com>
2025-11-29 21:55:12 +0530

    Add files via upload

---
+++ 52.DayTwentySixQuestionTwo.c
@@ -0,0 +1,47 @@
+//Q52: Write a program to print the following pattern:
+
+/* *
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+* */
+#include <stdio.h>
+
+int main() {
+
+    // First single star
+    printf("*\n\n");
+
+    // Second block - 2 stars
+    printf("*\n");
+    printf("*\n\n");
+
+    // Third block - 5 stars
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    // Fourth block - 3 stars
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    // Last single star
+    printf("*\n");
+
+    return 0;
+}

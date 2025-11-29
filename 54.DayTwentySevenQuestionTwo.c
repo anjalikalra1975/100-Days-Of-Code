18a3b72496acdfb86b8adcb011d39e58b9e7296d
anjalikalra1975 <anjali.kalraa2007@gmail.com>
2025-11-29 22:11:37 +0530

    Add files via upload

---
+++ 54.DayTwentySevenQuestionTwo.c
@@ -0,0 +1,44 @@
+//Q54 Write a program to print the following pattern:
+/*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 4; // Number of rows for upper half
+
+    // Upper half of the diamond
+    for (i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half of the diamond
+    for (i = n - 1; i >= 1; i--) {
+        // Print leading spaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
